ac1151305e1cf37aa69566a7d0efe931c56335bc
Ridwan Arif <ridwanarifpandiangan@gmail.com>
2025-07-16 18:35:35 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pembelajaran',
+  webDir: 'www'
+};
+
+export default config;
